6e5f10e4eb3361af8f845f5627681867d607d515
Mauricio Belduque Guzman <mbelduqu@bancolombia.com.co>
2023-03-24 19:53:29 -0500

    fix readme

--- src/main/java/com/nisum/api/controller/UserController.java
+++ src/main/java/com/nisum/api/controller/UserController.java
@@ -9,11 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +27,10 @@ public class UserController {
   @PostMapping
   public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO userRequestDTO) {
     return new ResponseEntity<>(userService.createUser(userRequestDTO.toModel()), HttpStatus.CREATED);
+  }
+
+  @PutMapping
+  public ResponseEntity<UserResponseDTO> updateUser(@RequestBody UserRequestDTO userRequestDTO) {
+    return new ResponseEntity<>(userService.updateUser(userRequestDTO.toModel()), HttpStatus.OK);
   }
 }
--- src/main/java/com/nisum/api/service/UserService.java
+++ src/main/java/com/nisum/api/service/UserService.java
@@ -1,5 +1,6 @@
 package com.nisum.api.service;
 
+import com.nisum.api.dto.request.UserRequestDTO;
 import com.nisum.api.dto.response.UserResponseDTO;
 import com.nisum.api.entity.UserEntity;
 import com.nisum.api.mapper.UserMapper;
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -57,6 +59,31 @@ public class UserService {
       });
     }
 
+    return util.setResponse(user);
+  }
+
+  public UserResponseDTO updateUser(User user) {
+    Optional<UserEntity> found = userRepository.findByEmail(user.getEmail());
+    if (found.isEmpty()) {
+      throw new IllegalArgumentException();
+    }
+    user.setId(found.get().getId());
+    user.setToken(UUID.randomUUID().toString());
+    user.setCreated(found.get().getCreated());
+    user.setModified(LocalDate.now());
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    User getUser = UserMapper.toDto(userRepository.save(UserMapper.toEntity(user)));
+    if (getUser != null) {
+      getUser.setPhones(user.getPhones());
+      user.getPhones().forEach(p -> {
+        Phone phones = new Phone();
+        phones.setCitycode(p.getCitycode());
+        phones.setCountrycode(p.getCountrycode());
+        phones.setNumber(p.getNumber());
+        phones.setUserId(getUser.getId());
+        phoneRepository.save(UserMapper.toModelPhoneDTO(phones));
+      });
+    }
     return util.setResponse(user);
   }
 }
